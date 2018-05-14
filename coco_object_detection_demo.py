2607297524a50a97679c7269994a7fb5fce3fc0b
Sevak Mardirosian <smardirosians@gmail.com>
2018-05-14 12:42:42 +0200

    lots of images. and working on report.

---
+++ coco_object_detection_demo.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
